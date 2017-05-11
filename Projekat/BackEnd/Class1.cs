b3e8c3e58f24332b6c548bcb55841a3c5abe91c6
Ahmed Oruc <aoruc1@etf.unsa.ba>
2017-05-11 21:40:59 +0200

    Dodana SignUp forma

--- Projekat/BackEnd/Class1.cs
+++ Projekat/BackEnd/Class1.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace BackEnd
 {
-    public class Class1
+    abstract class Osoba
     {
+        public string ime { get; set; }
+        public string prezime { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string eMail { get; set; }
+        public byte[] slika { get; set; }
+
+        public Osoba() { }
+        public Osoba(string Ime, string Prezime, string Username, string Password, string EMail, byte[] Slika)
+        {
+            ime = Ime;
+            prezime = Prezime;
+            username = Username;
+            password = Password;
+            eMail = EMail;
+            slika = Slika;
+        }
+
+
+        
     }
 }
