e7647b09d1f2023227e58c1b65fd50571f8f8a46
Dajana Mojsilović <dajana996@msn.com>
2017-05-12 23:20:30 +0200

    .

--- Projekat/TuristickaAgencija/App.xaml.cs
+++ Projekat/TuristickaAgencija/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using TuristickaAgencija.View;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
@@ -67,7 +68,7 @@ namespace TuristickaAgencija
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(SignUp), e.Arguments);
+                    rootFrame.Navigate(typeof(UnosPodataka), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
