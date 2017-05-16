d033e2956638d9ced27281c985544b7b2d2cbe6c
Ahmed Oruc <aoruc1@etf.unsa.ba>
2017-05-16 12:22:10 +0200

    Dodane klase

---
+++ Projekat/TuristickaAgencija/Klase/Osoba.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuristickaAgencija.Klase
+{
+    class Osoba
+    {
+    }
+}
