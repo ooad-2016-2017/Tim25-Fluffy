891e2f61cd60b5de25c2e42315a491e4191a3917
Ahmed Oruc <aoruc1@etf.unsa.ba>
2017-05-24 17:56:15 +0200

    Izmjene na namespace

--- Projekat/TuristickaAgencijaFluffy/TuristickaAgencijaFluffy/Model/TuristickaAgencija.cs
+++ Projekat/TuristickaAgencijaFluffy/TuristickaAgencijaFluffy/Model/TuristickaAgencija.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TuristickaAgencija.Model
+namespace TuristickaAgencijaFluffy.Model
 {
     class TuristickaAgencija
     {
--- Projekat/TuristickaAgencijaFluffy/TuristickaAgencijaFluffy/ViewModel/AdminViewModel.cs
+++ Projekat/TuristickaAgencijaFluffy/TuristickaAgencijaFluffy/ViewModel/AdminViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TuristickaAgencijaFluffy.Model;
 
 namespace TuristickaAgencijaFluffy.ViewModel
 {
     class AdminViewModel
     {
+        public Admin ad = new Admin();
     }
 }
